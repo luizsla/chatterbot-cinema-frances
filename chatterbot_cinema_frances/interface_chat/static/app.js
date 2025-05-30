42205fabf18c1fb897d6c779c80a53f375464d98
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-05-30 14:43:02 -0300

    Melhoria da lógica de JS

--- chatterbot_cinema_frances/interface_chat/static/app.js
+++ chatterbot_cinema_frances/interface_chat/static/app.js
@@ -3,11 +3,26 @@ const app = (() => {
         chat: $("#mensagem-chat-template"),
         user: $("#mensagem-user-template")
     }
+    const containerConversa = $("#conversa-chatterbot");
     const inputConversa = $("#conversa-input");
     const botaoEnviar = $("#conversa-enviar-btn");
 
     const perguntarRobo = pergunta => {
         console.log("A pergunta é: ", pergunta);
+
+        return "A pergunta é: " + pergunta;
+    }
+
+    const processarFala = (mensagemContainer, fala) => {
+        mensagemContainer.find(".texto-fala").text(fala);
+        containerConversa.append(mensagemContainer);
+    }
+
+    const processarDialogo = () => {
+        processarFala(templatesMensagens.user.clone(), inputConversa.val());
+        const resposta = perguntarRobo(inputConversa.val());
+        processarFala(templatesMensagens.chat.clone(), resposta);
+        inputConversa.val("");
     }
 
     return {
@@ -15,12 +30,10 @@ const app = (() => {
             inputConversa.on("keyup", event => {
                 if (event.which != 13) return;
 
-                perguntarRobo(inputConversa.val());
+                processarDialogo();
             });
 
-            botaoEnviar.on("click", () => {
-                perguntarRobo(inputConversa.val());
-            });
+            botaoEnviar.on("click", processarDialogo);
         }
     }
 })();
