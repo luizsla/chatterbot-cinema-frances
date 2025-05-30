fd6d263840dfcef21d68166cac679327b80a3a8e
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-05-30 13:39:15 -0300

    Adicionando comportamento de front-end ao chat

--- chatterbot_cinema_frances/interface_chat/static/app.js
+++ chatterbot_cinema_frances/interface_chat/static/app.js
@@ -1,1 +1,28 @@
-// alert("Estou sendo chamado!");
+const app = (() => {
+    const templatesMensagens = {
+        chat: $("#mensagem-chat-template"),
+        user: $("#mensagem-user-template")
+    }
+    const inputConversa = $("#conversa-input");
+    const botaoEnviar = $("#conversa-enviar-btn");
+
+    const perguntarRobo = pergunta => {
+        console.log("A pergunta é: ", pergunta);
+    }
+
+    return {
+        init() {
+            inputConversa.on("keyup", event => {
+                if (event.which != 13) return;
+
+                perguntarRobo(inputConversa.val());
+            });
+
+            botaoEnviar.on("click", () => {
+                perguntarRobo(inputConversa.val());
+            });
+        }
+    }
+})();
+
+$(app.init);
