cbdb6cb5adf73377a0d361b78887b6b4600119d3
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-05-30 13:08:23 -0300

    Adicioanndo interface web do chatbox com: 1 - Html5 + CSS + bootstrap4 2 - adicionando lógica JS em arquivo separado (nada de inline rs)

--- chatterbot_cinema_frances/interface_chat/chat.py
+++ chatterbot_cinema_frances/interface_chat/chat.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from chatterbot_cinema_frances.constantes import PORTA_INTERFACE_CHAT
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def mostrar_interface_chat():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
