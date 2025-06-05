f958c457fc75c8b9103a0876d3faf8298136cf9d
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-06-05 15:16:54 -0300

    1 - instalando pyyaml para fazer o parsing dos arquivos de corpus 2 - Iniciando um treinamento básico com corpus de "conversas" e "comprimentos" 3 - Fazendo script básico de treinamento 4 - Adicionando aruqivos de corpous

--- chatterbot_cinema_frances/treinamento_robo.py
+++ chatterbot_cinema_frances/treinamento_robo.py
@@ -1,16 +1,31 @@
-from chatterbot.trainers import ListTrainer
+import os
+
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 from chatterbot_cinema_frances.robo import iniciar_robo 
+
+_base_dir = os.path.dirname(__file__)
+
+_CORPUS_DATA = (
+    os.path.join(_base_dir, "dados_treinamento/comprimentos.yaml"),
+    os.path.join(_base_dir, "dados_treinamento/conversas.yaml"),
+)
+
+
+def _treinar_com_corpus_data(treinador): # https://github.com/gunthercox/chatterbot-corpus/tree/master/chatterbot_corpus/data/portuguese
+    for arquivo in _CORPUS_DATA:
+        treinador.train(arquivo)
+
+    print('Treinamento com arquivos de corpus finalizado!')
+
 
 
 def main():
     chatbot = iniciar_robo()
-    treinador = ListTrainer(chatbot)
+    treinador_lista = ListTrainer(chatbot)
+    treinador_corpus = ChatterBotCorpusTrainer(chatbot)
 
-    print("O treinador é", treinador, id(treinador))
-
-    
-
+    _treinar_com_corpus_data(treinador_corpus)
 
 
 if __name__ == "__main__":
