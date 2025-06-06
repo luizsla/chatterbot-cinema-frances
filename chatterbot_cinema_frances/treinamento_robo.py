76071d6cd8db8b05c607f3c82b98c29efa03bee5
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-06-06 13:01:45 -0300

    Adicionando mais treinamento por lista de strings

--- chatterbot_cinema_frances/treinamento_robo.py
+++ chatterbot_cinema_frances/treinamento_robo.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
@@ -10,6 +11,9 @@ _CORPUS_DATA = (
     os.path.join(_base_dir, "dados_treinamento/comprimentos.yaml"),
     os.path.join(_base_dir, "dados_treinamento/conversas.yaml"),
 )
+_LISTAS_TREINO = (
+    os.path.join(_base_dir, "dados_treinamento/solicitacoes_roteiro.json"),
+)
 
 
 def _treinar_com_corpus_data(treinador): # https://github.com/gunthercox/chatterbot-corpus/tree/master/chatterbot_corpus/data/portuguese
@@ -20,12 +24,25 @@ def _treinar_com_corpus_data(treinador): # https://github.com/gunthercox/chatter
 
 
 
+def _treinar_com_lista(treinador):
+    for arquivo in _LISTAS_TREINO:
+        with open(arquivo, 'r') as arquivo_aberto:
+            json_arquivo = json.load(arquivo_aberto)
+            for conversa in json_arquivo["conversas"]:
+                resposta = conversa["resposta"]
+                for mensagem in conversa["mensagens"]:
+                    treinador.train([mensagem, resposta])
+
+
+
 def main():
     chatbot = iniciar_robo()
+    chatbot.storage.drop()
     treinador_lista = ListTrainer(chatbot)
     treinador_corpus = ChatterBotCorpusTrainer(chatbot)
 
     _treinar_com_corpus_data(treinador_corpus)
+    _treinar_com_lista(treinador_lista)
 
 
 if __name__ == "__main__":
