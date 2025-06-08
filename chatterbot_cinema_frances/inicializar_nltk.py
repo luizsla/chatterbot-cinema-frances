0c558f07a7626003bb7312de40c85491558d1209
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-06-08 16:44:51 -0300

    Baixando e inicializando nltk e processando os artigos, já extraindo as 7 maiores palavras-chave de cada sinopse

---
+++ chatterbot_cinema_frances/inicializar_nltk.py
@@ -0,0 +1,13 @@
+import nltk
+
+
+def main():
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('floresta')
+
+
+if __name__ == "__main__":
+    main()
--- chatterbot_cinema_frances/processamento_sinopses.py
+++ chatterbot_cinema_frances/processamento_sinopses.py
@@ -1,10 +1,96 @@
 import os
+import sqlite3
 import yaml
+
+from collections import Counter
+from string import punctuation
+from nltk import word_tokenize, corpus
+from nltk.corpus import floresta
 
 
 _diretorio_arquivos = os.path.join(os.path.dirname(__file__), "sinopses_filmes")
 
 _NUMERO_MAX_ARTIGOS = 52
+
+_CLASSES_GRAMATICAIS_INDESEJADAS = (
+    "adv",
+    "v-inf", 
+    'v-fin',
+    "v-pcp",
+    "v-ger",
+    "num"
+)
+
+
+def _extrair_titulo(dados_sinopse):
+    return dados_sinopse["filme"]
+
+
+
+def _extrair_sinopse(dados_sinopse):
+    return dados_sinopse["sinopse"]
+
+
+
+def _extrair_info_adicional(dados_sinopse):
+    return "Diretor: %(diretor)s - Ano: %(ano)d - Roteiro: %(roteiro)s" % dados_sinopse
+
+
+
+def _eliminar_palavras_de_parada(tokens):
+    palavras_de_parada = set(corpus.stopwords.words("portuguese"))
+
+    return tuple(token for token in tokens if token not in palavras_de_parada)
+
+
+
+def _eliminar_caracteres_especiais(tokens):
+    return tuple(token for token in tokens if token not in punctuation)
+
+
+
+def _get_classificacoes_de_palavras():
+    classificacoes = {}
+    for palavra, classificacao in floresta.tagged_words():
+        classificacoes[palavra.lower()] = classificacao
+
+    return classificacoes
+
+
+
+def _eliminar_classes_gramaticais_indesejadas(tokens):
+    classificacoes = _get_classificacoes_de_palavras()
+
+    tokens_filtrados = []
+    for token in tokens:
+        if token in classificacoes:
+            classificacao = classificacoes[token]
+            if not any(s in classificacao for s in _CLASSES_GRAMATICAIS_INDESEJADAS):
+                tokens_filtrados.append(token)
+        else:
+            tokens_filtrados.append(token)
+
+    return tokens_filtrados
+
+
+
+def _filtrar_por_tokens_mais_presentes(tokens, n=7):
+    contador = Counter(tokens)
+    # Retorna os n tokens mais comuns e suas contagens
+    return tuple(palavra for palavra, vezes in contador.most_common(n))
+
+
+def _processar_sinopse_filme(sinopse):
+    tokens = word_tokenize(sinopse.lower())
+    tokens_sem_palavras_parada = _eliminar_palavras_de_parada(tokens)
+    tokens_sem_caracteres_especiais = _eliminar_caracteres_especiais(tokens_sem_palavras_parada)
+    tokens_gramaticalmente_limpos =  _eliminar_classes_gramaticais_indesejadas(
+        tokens_sem_caracteres_especiais
+    )
+    tokens_mais_frequentes = _filtrar_por_tokens_mais_presentes(tokens_gramaticalmente_limpos)
+
+    return tokens_mais_frequentes
+
 
 
 def main():
@@ -12,8 +98,11 @@ def main():
         arquivo_sinopse = os.path.join(_diretorio_arquivos, "%d.yaml" % contador)
         with open(arquivo_sinopse, 'r', encoding="utf-8") as arquivo:
             dados_sinopse = yaml.load(arquivo, Loader=yaml.SafeLoader)
-
-            print(dados_sinopse)
+            titulo = _extrair_titulo(dados_sinopse)
+            sinopse = _extrair_sinopse(dados_sinopse)
+            info_adicional = _extrair_info_adicional(dados_sinopse)
+            tokens_filme = _processar_sinopse_filme(sinopse)
+            assert len(tokens_filme) == 7, "Todas às sinopses devem ter 7 tags"
 
 
 if __name__ == "__main__":
